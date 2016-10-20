75cb07aa0a38b77248caf2632dae8a151410229d
Mario Sebasco <mariosebasco@yahoo.com>
2016-10-20 16:22:14 -0500

    Finished project.

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-Mario Sebasco - Computational Geometry Project 1
+#Mario Sebasco - Computational Geometry Project 1
+
+
+##Intro/Description
+
+The program `compGeoProj.py` is a python script which uses python 2.7.6 and the following three libraries:
+* math (standard math library)
+* cv2 (OpenCV is an open source image processing tool compatible with Python)
+* numpy (Extended math library for Python. Enables the use of matrices.)
+
+The program has several different functions that come together to produce the final result. 
+* create_blank: This function simply creates a blank image of (height,width) pixels. Please note that the height and width are defaulted to 1000. Changing this needs to be done in the source code.
+* lineGen: This function obtains a set of vertices and generates the polygon lines. It follows Bresenham's algorithm in order to generate them.
+* floodFill: Flood fill takes in a set of starting points and the image generated from lineGen and colors in the polygon give a specified color (BGR is the default for OpenCV, not RGB). It recursively looks outwards from where it is until it hits all the edges.
+* pointInPoly: This function is used in order to find a point inside a polygon (which is later used in floodFill). The function simply walks along the x-axis (at y_avg=(ymax+ymin)/2) until it finds the edge of the polygon, it the continues to walk until it lands inside the polygon and returns the set of points.
+*str2list: This small function is used in order to aid in the reading of the input text files. It transforms a list in string format to a numerical list.
+
+The overall program then is just a combination of these functions used to display the polygons, their union, and their intersections.
+
+
+##Running the code
+In order to run the code use the executable file `compGeoProj.py`. The terminal will prompt you to input the name of the text file consisting of the polygonal vertices. If input correctly the program will then generate the images (four if two polygons are involved: polygon1, polygon2, union, intersection). The folder comes with `sample1.txt` and `sample2.txt` as examples of what the user should expect to see.
+
+Please note the following restrictions for the program:
+* The text file must mimic that of the two sample text files provided
+* The program will only run if there are one or two polygons, it is not developed for more. Although implementation can be applied with relative ease if needed.
+* The vertices of the polygons must be integers. Although implementation of polygons with non-integer vertices can be implemented with relative ease if needed.
--- compGeoProj.py
+++ compGeoProj.py
@@ -3,6 +3,13 @@ import cv2
 import numpy as np
 import math
 
+####################################################################################################
+
+#Mario Sebasco - Computational Geometry Project
+
+####################################################################################################
+
+
 
 ######################################################################
 #First we create an empty image, which is just white
@@ -21,7 +28,7 @@ def lineGen(x,y,blank,unionPoly,interPoly):
     x.append(x[0])
     y.append(y[0])
     for i in range(len(x) - 1):
-        #first account for vertial line
+        #Account for vertical line
         if x[i] == x[i+1]:
             x[i+1] += 1
         #Account for horizontal lines
@@ -220,16 +227,21 @@ def floodFill(xstart,ystart,image,color,unionPoly,interPoly):
     
 
 ######################################################################
+#this last function finds a point in a polygon given a set of points
 def pointInPoly(xmin,xmax,ymin,ymax,image):
     """Function to find a point in a convex polygon"""
     yavg = int(math.floor((ymin + ymax)/2))
+    count = 0
     for i in range(xmin,xmax):
-        if image[yavg,i,0] == 0 and image[yavg,i,1] == 0 and image[yavg,i,2] == 0:
-            xInPoly = i + 1
-            yInPoly = yavg
-            break
-        else:
-            i += 1
+        #if you reached a black point
+        if image[yavg,i,0] == 0 and image[yavg,i,1] == 0 and image[yavg,i,2] == 0 or count == 1:
+            #walk to the nextpoint
+            count = 1
+            if image[yavg,i,0] == 255 and image[yavg,i,1] == 255 and image[yavg,i,2] == 255:
+                xInPoly = i
+                yInPoly = yavg
+                break
+
     return xInPoly, yInPoly
     
 
@@ -240,74 +252,250 @@ width, height = 1000, 1000
 unionPoly = create_blank(width,height)
 interPoly = create_blank(width,height)
 
-#Obtain Input
-xinput1 = [150,450,570,653,495,350,100]
-yinput1 = [300,300,525,780,845,790,345]
 
-xinput2 = [285,500,831,625,444,285]
-yinput2 = [420,225,543,695,710,555]
+#Ask for user input
+print('Please make sure that the format of your text file matches the sample text files provided.\n')
 
-sorted1 = []
-sorted2 = []
-sorted1y = []
-sorted2y = []
 
+inputFile = raw_input('What is the name of the text file with the polygon vertices (eg. input1.txt)?\n Or type "sample2.txt" to view the sample file\n')
+
+
+#Quick function used in converting the string file to the needed integers
+def str2list(str):
+    """convert a string input of a list and convert it to a numerical list"""
+    endIndex = str.find(')')
+    str = str[1:endIndex]
+    str = str.split(',')
+    count = 0
+    for item in str:
+        str[count] = int(item)
+        count += 1
+    return str
+############################################################################################################################################
+
+#set some default values
+numLines = 0
+numLines2 = 0
+count = 0
+xinput1 = []
+yinput1 = []
+xinput2 = []
+yinput2 = []
+color1 = (255,0,0)
+color2 = (0,255,0)
+
+#Read in vertices
+with open(inputFile,"r") as input:
+    for line in input:
+        numLines += 1
+        if line[0] == "P":
+            count += 1
+            numPoly = count
+    print('Your file has %d polygon(s)\n' % numPoly)
+
+with open(inputFile,"r") as input:
+    if numPoly == 2:
+        reachedPoly2 = False
+        for line in input:
+            if numLines == numLines2+1:
+                line = line+'**'
+            if line[0:2] == "P2":
+                reachedPoly2 = True
+            if not reachedPoly2:
+                if line[0] == "(":
+                    listLine = str2list(line)
+                    if len(listLine) == 3:
+                        color1 = listLine
+                    if len(listLine) == 2:
+                        xinput1.append(listLine[0])
+                        yinput1.append(listLine[1])
+
+            else:
+                if line[0] == "(":
+                    listLine = str2list(line)
+                    if len(listLine) == 3:
+                        color2 = listLine
+                    if len(listLine) == 2:
+                        xinput2.append(listLine[0])
+                        yinput2.append(listLine[1])
+            numLines2 += 1
+    if numPoly == 1:
+        color1 = (0,255,0)
+        for line in input:
+            if numLines == numLines2+1:
+                line = line+'**'
+            if line[0] == "(":
+                listLine = str2list(line)
+                xinput1.append(listLine[0])
+                yinput1.append(listLine[1])
+            numLines2 += 1
+
+############################################################################################################################################
+
+print('\nPlease wait a few seconds while the images are generated.\n')
+
+######################################################################
+#if there are two polygons then this chunk of code is run
 #Find polygon in front
-for i in xinput1:
-    sorted1.append(i)
+if numPoly ==2:
+    sorted1 = []
+    sorted2 = []
+    sorted1y = []
+    sorted2y = []
+
+    for i in xinput1:
+        sorted1.append(i)
+
+    for i in yinput1:
+        sorted1y.append(i)
+
+    for i in xinput2:
+        sorted2.append(i)
+
+    for i in yinput2:
+        sorted2y.append(i)
+
+    sorted1.sort()
+    sorted2.sort()
+    sorted1y.sort()
+    sorted2y.sort()
+
+    xmin1 = sorted1[0]
+    xmax1 = sorted1[-1]
+    ymin1 = sorted1y[0]
+    ymax1 = sorted1y[-1]
+
+    xmin2 = sorted2[0]
+    xmax2 = sorted2[-1]
+    ymin2 = sorted2y[0]
+    ymax2 = sorted2y[-1]
+
+    if sorted1[0] <= sorted2[0]:
+        poly1IsFirst = True
+    else:
+        poly1IsFirst = False
+
+    if poly1IsFirst:
+        #draw polygon 2 then draw polygon 1 (in the intersection image)
+        ####################################################################
+        #Polygon 2
+        image2 = create_blank(width,height)
+
+        #Draw edges
+        lineImage2, unionPoly, interPoly = lineGen(xinput2,yinput2,image2,unionPoly,interPoly)
+
+        #Find point inside polygon
+        pointInPoly2x, pointInPoly2y = pointInPoly(xmin2,xmax2,ymin2,ymax2,lineImage2)
+
+        #flood fill the polygon
+        floodImage2, unionPoly,interPoly = floodFill(pointInPoly2y,pointInPoly2x,lineImage2,color2,unionPoly,interPoly)
+
+        #image2 = cv2.imwrite('poly_image2.jpg', floodImage2)
+        #img2 = cv2.imread('poly_image2.jpg',1)
+        cv2.imshow("Polygon2",floodImage2)
+
+        ######################################################################
+        #Polygon 1
+        image1 = create_blank(width, height)
+
+        #Draw edges
+        lineImage1, unionPoly,interPoly = lineGen(xinput1,yinput1,image1,unionPoly,interPoly)
+
+        #Find point in polygon
+        pointInPoly1x, pointInPoly1y = pointInPoly(xmin1,xmax1,ymin1,ymax1,lineImage1)
+
+        #flood fill the polygon
+        floodImage1, unionPoly, interPoly = floodFill(pointInPoly1y,pointInPoly1x,lineImage1,color1,unionPoly,interPoly)
+ 
+        #image1 = cv2.imwrite('poly_image1.jpg', gray_image)
+        #img1 = cv2.imread('poly_image1.jpg',1)
+        cv2.imshow("Polygon1",floodImage1)
+
+
+        ######################################################################
+        #Find the Union
+        cv2.imshow("Polygon_Union",unionPoly)
+
+        ######################################################################
+        #find the intersection
+        cv2.imshow("Polygon_Intersection",interPoly)
+
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    else:
+        #draw polygon 1 then draw polygon 2
+        ######################################################################
+        #Polygon 1
+        image1 = create_blank(width, height)
+
+        #Draw edges
+        lineImage1, unionPoly, interPoly = lineGen(xinput1,yinput1,image1,unionPoly,interPoly)
+
+        #Find point inside polygon
+        pointInPoly1x, pointInPoly1y = pointInPoly(xmin1,xmax1,ymin1,ymax1,lineImage1)
+
+        #flood fill the polygon
+        floodImage1, unionPoly, interPoly = floodFill(pointInPoly1y,pointInPoly1x,lineImage1,color1,unionPoly,interPoly)
+
+        #image1 = cv2.imwrite('poly_image1.jpg', gray_image)
+        #img1 = cv2.imread('poly_image1.jpg',1)
+        cv2.imshow("Polygon1",floodImage1)
+
+
+        ####################################################################
+        #Polygon 2
+        image2 = create_blank(width,height)
+
+        #Draw edges
+        lineImage2, unionPoly, interPoly = lineGen(xinput2,yinput2,image2,unionPoly, interPoly)
+
+        #Find point inside polygon
+        pointInPoly2x, pointInPoly2y = pointInPoly(xmin2,xmax2,ymin2,ymax2,lineImage2)
+
+        #flood fill the polygon
+        floodImage2, unionPoly, interPoly = floodFill(pointInPoly2y,pointInPoly2x,lineImage2,color2,unionPoly,interPoly)
+
+        #image2 = cv2.imwrite('poly_image2.jpg', floodImage2)
+        #img2 = cv2.imread('poly_image2.jpg',1)
+        cv2.imshow("Polygon2",floodImage2)
+
+        ######################################################################
+        #Find the Union
+        cv2.imshow("Polygon_Union",unionPoly)
+
+        ######################################################################
+        #find the intersection
+        cv2.imshow("Polygon_Intersection",interPoly)
+
+
+
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+
+######################################################################
+#If the number of polygons is 1, then this is prompted
+if numPoly == 1:
+
+    sorted1 = []
+    sorted1y = []
+
+    for i in xinput1:
+        sorted1.append(i)
+
+    for i in yinput1:
+        sorted1y.append(i)
+
+    sorted1.sort()
+    sorted1y.sort()
+
+    xmin1 = sorted1[0]
+    xmax1 = sorted1[-1]
+    ymin1 = sorted1y[0]
+    ymax1 = sorted1y[-1]
+
     
-for i in yinput1:
-    sorted1y.append(i)
-
-for i in xinput2:
-    sorted2.append(i)
-
-for i in yinput2:
-    sorted2y.append(i)
-    
-sorted1.sort()
-sorted2.sort()
-sorted1y.sort()
-sorted2y.sort()
-
-xmin1 = sorted1[0]
-xmax1 = sorted1[-1]
-ymin1 = sorted1y[0]
-ymax1 = sorted1y[-1]
-
-xmin2 = sorted2[0]
-xmax2 = sorted2[-1]
-ymin2 = sorted2y[0]
-ymax2 = sorted2y[-1]
-
-if sorted1[0] <= sorted2[0]:
-    poly1IsFirst = True
-else:
-    poly1IsFirst = False
-
-if poly1IsFirst:
-    #draw polygon 2 then draw polygon 1 (in the intersection image)
-    ####################################################################
-    #Polygon 2
-    image2 = create_blank(width,height)
-
-    #Draw edges
-    lineImage2, unionPoly, interPoly = lineGen(xinput2,yinput2,image2,unionPoly,interPoly)
-
-    #Find point inside polygon
-    pointInPoly2x, pointInPoly2y = pointInPoly(xmin2,xmax2,ymin2,ymax2,lineImage2)
-
-    #flood fill the polygon
-    color2 = (0,255,0)
-    floodImage2, unionPoly,interPoly = floodFill(pointInPoly2y,pointInPoly2x,lineImage2,color2,unionPoly,interPoly)
-    #grayImage2 = cv2.cvtColor(floodImage2,cv2.COLOR_BGR2GRAY)
-    #(thresh, bwImage2) = cv2.threshold(grayImage2, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-
-    #image2 = cv2.imwrite('poly_image2.jpg', floodImage2)
-    #img2 = cv2.imread('poly_image2.jpg',1)
-    cv2.imshow("Polygon2",floodImage2)
-
-    ######################################################################
     #Polygon 1
     image1 = create_blank(width, height)
 
@@ -318,83 +506,12 @@ if poly1IsFirst:
     pointInPoly1x, pointInPoly1y = pointInPoly(xmin1,xmax1,ymin1,ymax1,lineImage1)
     
     #flood fill the polygon
-    color1 = (255,0,0)
     floodImage1, unionPoly, interPoly = floodFill(pointInPoly1y,pointInPoly1x,lineImage1,color1,unionPoly,interPoly)
-    #grayImage1 = cv2.cvtColor(floodImage1,cv2.COLOR_BGR2GRAY)
-    #(thresh, bwImage1) = cv2.threshold(grayImage1, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+
 
     #image1 = cv2.imwrite('poly_image1.jpg', gray_image)
     #img1 = cv2.imread('poly_image1.jpg',1)
     cv2.imshow("Polygon1",floodImage1)
     
-
-    ######################################################################
-    #Find the Union
-    cv2.imshow("Polygon_Union",unionPoly)
-
-    ######################################################################
-    #find the intersection
-    cv2.imshow("Polygon_Intersection",interPoly)
-
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-
-else:
-    #draw polygon 1 then draw polygon 2
-    ######################################################################
-    #Polygon 1
-    image1 = create_blank(width, height)
-
-    #Draw edges
-    lineImage1, unionPoly, interPoly = lineGen(xinput1,yinput1,image1,unionPoly,interPoly)
-
-    #Find point inside polygon
-    pointInPoly1x, pointInPoly1y = pointInPoly(xmin1,xmax1,ymin1,ymax1,lineImage1)
-
-    #flood fill the polygon
-    color1 = (255,0,0)
-    floodImage1, unionPoly, interPoly = floodFill(pointInPoly1y,pointInPoly1x,lineImage1,color1,unionPoly,interPoly)
-    #grayImage1 = cv2.cvtColor(floodImage1,cv2.COLOR_BGR2GRAY)
-    #(thresh, bwImage1) = cv2.threshold(grayImage1, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-
-    #image1 = cv2.imwrite('poly_image1.jpg', gray_image)
-    #img1 = cv2.imread('poly_image1.jpg',1)
-    cv2.imshow("Polygon1",floodImage1)
-
-
-    ####################################################################
-    #Polygon 2
-    image2 = create_blank(width,height)
-
-    #Draw edges
-    lineImage2, unionPoly, interPoly = lineGen(xinput2,yinput2,image2,unionPoly, interPoly)
-
-    #Find point inside polygon
-    pointInPoly2x, pointInPoly2y = pointInPoly(xmin2,xmax2,ymin2,ymax2,lineImage2)
-
-    #flood fill the polygon
-    color2 = (0,255,0)
-    floodImage2, unionPoly, interPoly = floodFill(pointInPoly2y,pointInPoly2x,lineImage2,color2,unionPoly,interPoly)
-    #grayImage2 = cv2.cvtColor(floodImage2,cv2.COLOR_BGR2GRAY)
-    #(thresh, bwImage2) = cv2.threshold(grayImage2, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-
-    #image2 = cv2.imwrite('poly_image2.jpg', floodImage2)
-    #img2 = cv2.imread('poly_image2.jpg',1)
-    cv2.imshow("Polygon2",floodImage2)
-
-    ######################################################################
-    #Find the Union
-    cv2.imshow("Polygon_Union",unionPoly)
-
-    ######################################################################
-    #find the intersection
-    cv2.imshow("Polygon_Intersection",interPoly)
-
-
-    
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-
-
-
